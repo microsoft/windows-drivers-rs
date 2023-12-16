f6f6470d2e0427462744e61770b5e5d7926bb98a
Dr. Chat <arkolbed@gmail.com>
2023-12-16 14:41:33 -0600

    Change `call_unsafe_wdf_function_binding` syntax to look more like a function call

--- crates/wdk-macros/tests/non-nightly/trybuild/wdf_api_that_does_not_exist.rs
+++ crates/wdk-macros/tests/non-nightly/trybuild/wdf_api_that_does_not_exist.rs
@@ -4,15 +4,14 @@
 #![no_main]
 use wdk_sys::*;
 
-#[export_name = "DriverEntry"]// WDF expects a symbol with the name DriverEntry
+#[export_name = "DriverEntry"] // WDF expects a symbol with the name DriverEntry
 pub extern "system" fn driver_entry(
     driver: &mut DRIVER_OBJECT,
     registry_path: PCUNICODE_STRING,
 ) -> NTSTATUS {
     unsafe {
-        wdk_macros::call_unsafe_wdf_function_binding!(
-            WdfApiThatDoesNotExist,
-            driver as PDRIVER_OBJECT,
-        )
+        wdk_macros::call_unsafe_wdf_function_binding! {
+            WdfApiThatDoesNotExist(driver as PDRIVER_OBJECT)
+        }
     }
 }
