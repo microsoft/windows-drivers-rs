362679a8f751cfeefccdf7587e4c25a0fbfc613f
Melvin Wang <melvin.mc.wang@gmail.com>
2024-06-02 13:06:26 -0700

    fix missing copyrights

--- tests/config-kmdf/tests/call_unsafe_wdf_function_binding_tests.rs
+++ tests/config-kmdf/tests/call_unsafe_wdf_function_binding_tests.rs
@@ -1,3 +1,6 @@
+// Copyright (c) Microsoft Corporation
+// License: MIT OR Apache-2.0
+
 #[cfg(test)]
 mod tests {
     wdk_macros_tests::generate_call_unsafe_wdf_binding_tests!();
--- tests/kmdf-driver-workspace/crates/driver_1/src/lib.rs
+++ tests/kmdf-driver-workspace/crates/driver_1/src/lib.rs
@@ -1,14 +1,2 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+// Copyright (c) Microsoft Corporation
+// License: MIT OR Apache-2.0
