5e427e7e1547531b7efb6d7c94058ef48341129b
NateD-MSFT <34494373+NateD-MSFT@users.noreply.github.com>
2024-07-17 20:11:12 -0700

    Fix clippy issues in test and add cases

--- crates/wdk-sys/src/lib.rs
+++ crates/wdk-sys/src/lib.rs
@@ -134,14 +134,22 @@ mod tests {
         STATUS_SUCCESS,
     };
     #[test]
-    pub fn status_tests() {
-        const {
-            assert!(NT_SUCCESS(STATUS_SUCCESS));
-            assert!(NT_SUCCESS(STATUS_HIBERNATED));
-            assert!(NT_INFORMATION(STATUS_HIBERNATED));
-            assert!(NT_WARNING(STATUS_BREAKPOINT));
-            assert!(NT_ERROR(STATUS_PRIVILEGED_INSTRUCTION));
-            assert!(!(NT_INFORMATION(STATUS_SUCCESS)));
-        }
+    pub const fn nt_status_validation() {
+        assert!(NT_SUCCESS(STATUS_SUCCESS));
+        assert!(NT_SUCCESS(STATUS_HIBERNATED));
+        assert!(NT_INFORMATION(STATUS_HIBERNATED));
+        assert!(NT_WARNING(STATUS_BREAKPOINT));
+        assert!(NT_ERROR(STATUS_PRIVILEGED_INSTRUCTION));
+        assert!(!NT_SUCCESS(STATUS_BREAKPOINT));
+        assert!(!NT_SUCCESS(STATUS_PRIVILEGED_INSTRUCTION));
+        assert!(!NT_INFORMATION(STATUS_SUCCESS));
+        assert!(!NT_INFORMATION(STATUS_BREAKPOINT));
+        assert!(!NT_INFORMATION(STATUS_PRIVILEGED_INSTRUCTION));
+        assert!(!NT_WARNING(STATUS_SUCCESS));
+        assert!(!NT_WARNING(STATUS_HIBERNATED));
+        assert!(!NT_WARNING(STATUS_PRIVILEGED_INSTRUCTION));
+        assert!(!NT_ERROR(STATUS_SUCCESS));
+        assert!(!NT_ERROR(STATUS_HIBERNATED));
+        assert!(!NT_ERROR(STATUS_BREAKPOINT));
     }
 }
