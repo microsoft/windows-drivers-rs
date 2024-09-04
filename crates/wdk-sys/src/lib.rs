372b40b206e254222d440b2bb4a2322cdbba2908
NateD-MSFT <34494373+NateD-MSFT@users.noreply.github.com>
2024-09-04 16:46:51 -0700

    Factor out wdk_sys NT_SUCCESS tests to a new folder.

--- crates/wdk-sys/src/lib.rs
+++ crates/wdk-sys/src/lib.rs
@@ -149,36 +149,3 @@ macro_rules! PAGED_CODE {
         debug_assert!(unsafe { KeGetCurrentIrql() <= APC_LEVEL as u8 });
     };
 }
-
-#[cfg(test)]
-mod tests {
-    use crate::{
-        NT_ERROR,
-        NT_INFORMATION,
-        NT_SUCCESS,
-        NT_WARNING,
-        STATUS_BREAKPOINT,
-        STATUS_HIBERNATED,
-        STATUS_PRIVILEGED_INSTRUCTION,
-        STATUS_SUCCESS,
-    };
-    #[test]
-    pub const fn nt_status_validation() {
-        assert!(NT_SUCCESS(STATUS_SUCCESS));
-        assert!(NT_SUCCESS(STATUS_HIBERNATED));
-        assert!(NT_INFORMATION(STATUS_HIBERNATED));
-        assert!(NT_WARNING(STATUS_BREAKPOINT));
-        assert!(NT_ERROR(STATUS_PRIVILEGED_INSTRUCTION));
-        assert!(!NT_SUCCESS(STATUS_BREAKPOINT));
-        assert!(!NT_SUCCESS(STATUS_PRIVILEGED_INSTRUCTION));
-        assert!(!NT_INFORMATION(STATUS_SUCCESS));
-        assert!(!NT_INFORMATION(STATUS_BREAKPOINT));
-        assert!(!NT_INFORMATION(STATUS_PRIVILEGED_INSTRUCTION));
-        assert!(!NT_WARNING(STATUS_SUCCESS));
-        assert!(!NT_WARNING(STATUS_HIBERNATED));
-        assert!(!NT_WARNING(STATUS_PRIVILEGED_INSTRUCTION));
-        assert!(!NT_ERROR(STATUS_SUCCESS));
-        assert!(!NT_ERROR(STATUS_HIBERNATED));
-        assert!(!NT_ERROR(STATUS_BREAKPOINT));
-    }
-}
