c0eeeb8ea8a63beaa7800a93899e9a496b32043e
Shravan Vasista <svasista@microsoft.com>
2024-10-09 21:56:36 -0700

    List contents of WDK Root Dir

--- crates/wdk-build/src/utils.rs
+++ crates/wdk-build/src/utils.rs
@@ -14,7 +14,13 @@ use thiserror::Error;
 use windows::{
     core::{s, PCSTR},
     Win32::System::Registry::{
-        RegCloseKey, RegGetValueA, RegOpenKeyExA, HKEY, HKEY_LOCAL_MACHINE, KEY_READ, RRF_RT_REG_SZ,
+        RegCloseKey,
+        RegGetValueA,
+        RegOpenKeyExA,
+        HKEY,
+        HKEY_LOCAL_MACHINE,
+        KEY_READ,
+        RRF_RT_REG_SZ,
     },
 };
 
@@ -424,7 +430,9 @@ mod tests {
 
     mod read_registry_key_string_value {
         use windows::Win32::UI::Shell::{
-            FOLDERID_ProgramFiles, SHGetKnownFolderPath, KF_FLAG_DEFAULT,
+            FOLDERID_ProgramFiles,
+            SHGetKnownFolderPath,
+            KF_FLAG_DEFAULT,
         };
 
         use super::*;
