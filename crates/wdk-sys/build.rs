0243f685be485d7893b509bd31037bbfcd66a1dc
Leon Durrenberger <leondu@microsoft.com>
2024-12-04 16:10:49 -0800

    fix newline around format macro

--- crates/wdk-sys/build.rs
+++ crates/wdk-sys/build.rs
@@ -48,7 +48,8 @@ const WDF_FUNCTION_COUNT_DECLARATION_TABLE_INDEX: &str = "
         let wdf_function_count = crate::_WDFFUNCENUM::WdfFunctionTableNumEntries as usize;";
 
 static WDF_FUNCTION_TABLE_TEMPLATE: LazyLock<String> = LazyLock::new(|| {
-    format!(r"
+    format!(
+        r"
 extern crate alloc;
 
 /// Struct to encapsulate a function table.
