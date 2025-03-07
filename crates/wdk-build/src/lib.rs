3973f1f0cb00cbb1c52ef7417a981bbb3fa0d4a2
Wedson Almeida Filho <walmeida@microsoft.com>
2025-03-07 11:42:31 -0300

    Add `fltkernel.h` to base headers
    This allows rust drivers to use fltmgr.

--- crates/wdk-build/src/lib.rs
+++ crates/wdk-build/src/lib.rs
@@ -683,7 +683,7 @@ impl Config {
     fn base_headers(&self) -> Vec<&'static str> {
         match &self.driver_config {
             DriverConfig::Wdm | DriverConfig::Kmdf(_) => {
-                vec!["ntifs.h", "ntddk.h", "ntstrsafe.h"]
+                vec!["ntifs.h", "ntddk.h", "ntstrsafe.h", "fltkernel.h"]
             }
             DriverConfig::Umdf(_) => {
                 vec!["windows.h"]
@@ -954,6 +954,7 @@ impl Config {
                 println!("cargo::rustc-link-lib=static=ntoskrnl");
                 println!("cargo::rustc-link-lib=static=hal");
                 println!("cargo::rustc-link-lib=static=wmilib");
+                println!("cargo::rustc-link-lib=static=fltmgr");
 
                 // Emit ARM64-specific libraries to link to derived from
                 // WindowsDriver.arm64.props
@@ -988,6 +989,7 @@ impl Config {
                 println!("cargo::rustc-link-lib=static=wmilib");
                 println!("cargo::rustc-link-lib=static=WdfLdr");
                 println!("cargo::rustc-link-lib=static=WdfDriverEntry");
+                println!("cargo::rustc-link-lib=static=fltmgr");
 
                 // Emit ARM64-specific libraries to link to derived from
                 // WindowsDriver.arm64.props
@@ -1461,6 +1463,7 @@ mod tests {
                 r#"#include "ntifs.h"
 #include "ntddk.h"
 #include "ntstrsafe.h"
+#include "fltkernel.h"
 "#,
             );
         }
@@ -1481,6 +1484,7 @@ mod tests {
                 r#"#include "ntifs.h"
 #include "ntddk.h"
 #include "ntstrsafe.h"
+#include "fltkernel.h"
 #include "wdf.h"
 "#,
             );
