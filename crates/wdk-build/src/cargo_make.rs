1c4e80ff2a4e9b7f8f1b0662cda6df95530dedd2
leon-xd <leondu@microsoft.com>
2025-03-20 15:24:36 -0700

    clippy nightly passing

--- crates/wdk-build/src/cargo_make.rs
+++ crates/wdk-build/src/cargo_make.rs
@@ -977,8 +977,7 @@ pub fn package_driver_flow_condition_script() -> anyhow::Result<()> {
         if !current_package.targets.iter().any(|target| {
             target
                 .kind
-                .iter()
-                .any(|kind| *kind == cargo_metadata::TargetKind::CDyLib)
+                .contains(&cargo_metadata::TargetKind::CDyLib)
         }) {
             return Err::<(), anyhow::Error>(
                 metadata::TryFromCargoMetadataError::NoWdkConfigurationsDetected.into(),
