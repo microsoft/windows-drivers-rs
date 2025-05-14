071895b98168f5fd4d87ea6caa0f2a1c2ee1f89c
Zero Tang <zero.tangptr@gmail.com>
2025-05-14 21:09:41 +0800

    Add samples to verify the alignments
    Also address Melvin's reviews.

--- crates/wdk-alloc/src/lib.rs
+++ crates/wdk-alloc/src/lib.rs
@@ -28,15 +28,10 @@ pub use kernel_mode::*;
 #[cfg(any(driver_model__driver_type = "WDM", driver_model__driver_type = "KMDF"))]
 mod kernel_mode {
 
-    use core::{
-        alloc::{GlobalAlloc, Layout},
-        sync::atomic::{AtomicU32, Ordering},
-    };
+    use core::alloc::{GlobalAlloc, Layout};
 
     use wdk_sys::{
-        ntddk::{ExAllocatePool2, ExFreePoolWithTag},
-        POOL_FLAG_NON_PAGED,
-        SIZE_T,
+        ntddk::{ExAllocatePool2, ExFreePoolWithTag}, PAGE_SIZE, POOL_FLAG_NON_PAGED, PVOID, SIZE_T, ULONG
     };
 
     /// Allocator implementation to use with `#[global_allocator]` to allow use
@@ -47,14 +42,20 @@ mod kernel_mode {
     /// <= `DISPATCH_LEVEL`
     pub struct WdkAllocator;
 
-    /// The value of memory tags are stored in little-endian order, so it is
-    /// convenient to reverse the order for readability in tooling (ie. Windbg).
-    /// The default tag value is `rust`. You may use `store` method to mutate its value.
-    /// However, it's strongly recommended that you mutate the tag for only once.
-    pub static GLOBAL_POOL_TAG: AtomicU32 = AtomicU32::new(u32::from_ne_bytes(*b"rust"));
+    // The value of memory tags are stored in little-endian order, so it is
+    // convenient to reverse the order for readability in tooling (ie. Windbg).
+    const RUST_TAG: ULONG = u32::from_ne_bytes(*b"rust");
     
     // The minimum alignment of pointer returned by ExAllocatePool2.
     const POOL_ALIGNMENT: usize = size_of::<*mut u8>() * 2;
+
+    #[inline] fn require_realignment(layout: Layout) -> bool {
+        if layout.align() <= POOL_ALIGNMENT {
+            false
+        } else {
+            layout.align() > PAGE_SIZE as usize || layout.size() < PAGE_SIZE as usize
+        }
+    }
 
     // SAFETY: This is safe because the Wdk allocator:
     //         1. can never unwind since it can never panic
@@ -64,26 +65,30 @@ mod kernel_mode {
         unsafe fn alloc(&self, layout: Layout) -> *mut u8 {
             let ptr =
                 // SAFETY: `ExAllocatePool2` is safe to call from any `IRQL` <= `DISPATCH_LEVEL` since its allocating from `POOL_FLAG_NON_PAGED`
-                unsafe {
-                    if layout.align() > POOL_ALIGNMENT {
-                        // If the requested alignment is too large, we'll have to waste some space as large as the alignment.
-                        let size = layout.size() + layout.align();
-                        let mask = !(layout.align() - 1);
-                        let p = ExAllocatePool2(POOL_FLAG_NON_PAGED, size as SIZE_T, GLOBAL_POOL_TAG.load(Ordering::Relaxed));
-                        if !p.is_null() {
-                            let q = ((p as usize & mask) + layout.align()) as *mut *mut u8;
-                            // Store the original pointer right before the pointer to return,
-                            // so that ExFreePoolWithTag can receive the correct pointer.
-                            // This is also how `_aligned_malloc` is implemented in msvcrt.dll
-                            q.sub(1).write(p.cast());
-                            q.cast()
-                        } else {
-                            p
+                if require_realignment(layout) {
+                    // If the requested alignment is too large, we'll have to waste some space as large as the alignment.
+                    let size = layout.size() + layout.align();
+                    let mask = !(layout.align() - 1);
+                    let p = unsafe {
+                        ExAllocatePool2(POOL_FLAG_NON_PAGED, size as SIZE_T, RUST_TAG)
+                    };
+                    if !p.is_null() {
+                        let q = ((p as usize & mask) + layout.align()) as *mut PVOID;
+                        // Store the original pointer right before the pointer to return,
+                        // so that ExFreePoolWithTag can receive the correct pointer.
+                        // This is also how `_aligned_malloc` is implemented in msvcrt.dll
+                        unsafe {
+                            q.sub(1).write(p);
                         }
+                        q.cast()
                     } else {
-                        // Because we know the alignment while in both alloc and dealloc,
-                        // we don't need to waste space if the default alignment is small enough.
-                        ExAllocatePool2(POOL_FLAG_NON_PAGED, layout.size() as SIZE_T, GLOBAL_POOL_TAG.load(Ordering::Relaxed))
+                        p
+                    }
+                } else {
+                    // Because we know the alignment while in both alloc and dealloc,
+                    // we don't need to waste space if the default alignment is small enough.
+                    unsafe {
+                        ExAllocatePool2(POOL_FLAG_NON_PAGED, layout.size() as SIZE_T, RUST_TAG)
                     }
                 };
             if ptr.is_null() {
@@ -95,17 +100,19 @@ mod kernel_mode {
         unsafe fn dealloc(&self, ptr: *mut u8, layout: Layout) {
             // SAFETY: `ExFreePool` is safe to call from any `IRQL` <= `DISPATCH_LEVEL`
             // since its freeing memory allocated from `POOL_FLAG_NON_PAGED` in `alloc`
-            unsafe {
-                let p = if layout.align() > POOL_ALIGNMENT {
-                    // The alignment is too large, the original pointer is stored right before the ptr.
-                    // This is also how `_aligned_free` is implemented in msvcrt.dll
-                    let q: *mut *mut u8 = ptr.cast();
+            let p = if require_realignment(layout) {
+                // The alignment is too large, the original pointer is stored right before
+                // the ptr. This is also how `_aligned_free` is implemented in msvcrt.dll
+                let q: *mut *mut u8 = ptr.cast();
+                unsafe {
                     q.sub(1).read()
-                } else {
-                    // The alignment is normal, so the ptr is already the original pointer.
-                    ptr.cast()
-                };
-                ExFreePoolWithTag(p.cast(), GLOBAL_POOL_TAG.load(Ordering::Relaxed));
+                }
+            } else {
+                // The alignment is normal, so the ptr is already the original pointer.
+                ptr.cast()
+            };
+            unsafe {
+                ExFreePoolWithTag(p.cast(), RUST_TAG);
             }
         }
     }
