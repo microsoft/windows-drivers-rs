5d2fc8de8d608d8b766126430aa92d9e74a005d0
Ladi Prosek <laprosek@microsoft.com>
2025-08-04 16:22:27 +0200

    Fix formatting

--- crates/wdk-sys/src/lib.rs
+++ crates/wdk-sys/src/lib.rs
@@ -134,8 +134,9 @@ pub extern "system" fn __CxxFrameHandler3() -> i32 {
 #[allow(clippy::missing_const_for_fn)] // const extern is not yet supported: https://github.com/rust-lang/rust/issues/64926
 #[no_mangle]
 pub extern "system" fn __CxxFrameHandler4() -> i32 {
-    // This is a stub for the C++ exception handling frame handler. It's never called but it needs to be distinct
-    // from __CxxFrameHandler3 to not confuse binary analysis tools. We return a different value to prevent folding.
+    // This is a stub for the C++ exception handling frame handler. It's never
+    // called but it needs to be distinct from __CxxFrameHandler3 to not confuse
+    // binary analysis tools. We return a different value to prevent folding.
     1
 }
 
@@ -144,9 +145,10 @@ pub extern "system" fn __CxxFrameHandler4() -> i32 {
 #[allow(clippy::missing_const_for_fn)] // const extern is not yet supported: https://github.com/rust-lang/rust/issues/64926
 #[no_mangle]
 pub extern "system" fn __GSHandlerCheck_EH4() -> i32 {
-    // This is a stub for the C++ exception handling frame handler. It's never called but it needs to be distinct
-    // from __CxxFrameHandler3 and __CxxFrameHandler4 to not confuse binary analysis tools. We return a different
-    // value to prevent folding.
+    // This is a stub for the C++ exception handling frame handler. It's never
+    // called but it needs to be distinct from __CxxFrameHandler3 and
+    // __CxxFrameHandler4 to not confuse binary analysis tools. We return a
+    // different value to prevent folding.
     2
 }
 
