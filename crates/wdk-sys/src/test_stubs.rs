8de321be6eb9ec4c635f86baeba5680bcf9951b0
Alan Ngo <ngoalan@microsoft.com>
2026-07-29 16:01:12 -0700

    - add doc comments to test_stubs.rs

--- crates/wdk-sys/src/test_stubs.rs
+++ crates/wdk-sys/src/test_stubs.rs
@@ -4,6 +4,37 @@
 //! Any library dependency that depends on `wdk-sys` requires these stubs to
 //! provide symbols to successfully compile and run tests.
 //!
+//! Some scenarios where these stubs can be helpful:
+//!
+//! - Default cargo profiles
+//!
+//! - `wdk-sys` dependent crates that need to enable compilation for `test`
+//!   targets
+//!   - enabling these stubs bypasses the need to manually define symbols
+//!     expected to be present by the bindings (usually symbols provided by the
+//!     final binary like `DriverEntry`)
+//!
+//! - lib crate usage that depends on wdk-sys, eg. not a driver bin crate
+//!   - if used with a driver bin crate you may need to cfg gate your
+//!     DriverEntry since these stubs provide one
+//!
+//! - crate tests that don't rely on the stubbed symbols' functionality
+//!   - if you want to write tests where these symbols are exercised (ex: tests
+//!     that call WDF functions via our macros) then you must provide your own
+//!     mocks in the test
+//!
+//! NOTE: Enabling fat LTO in your dev profile may lead to Linker errors
+//! even if you sufficiently cfg gate WDF function usage. This is because
+//! the dev profile defaults to `opt-level = 0`, and in combination with fat
+//! LTO may cause dead code to not be optimized out (fat LTO merges all upstream
+//! crates' code into the final binary rather than letting the linker pull in
+//! only what's needed, so WDF wrappers from an ungated `use wdk::` get
+//! included even if you never call them).
+//!   - If "test-stubs" is enabled the Linker may complain that
+//!     `WdfDriverGlobals` is missing, it is intentionally not stubbed here
+//!     because it is needed when a WDF function is called, landing it outside
+//!     the intent of test-stubs.
+//!
 //! These stubs can be brought into scope by introducing `wdk-sys` with the
 //! `test-stubs` feature in the `dev-dependencies` of the crate's `Cargo.toml`
 
